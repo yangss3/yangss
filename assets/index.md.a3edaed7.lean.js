faf8d802ad32596a35be3932212a362d0ca9cb1d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-09-22 08:27:14 +0000

    Deploy yangss3/yangss to github.com/yangss3/yangss.git:gh-pages

--- assets/index.md.a3edaed7.lean.js
+++
@@ -1,1 +0,0 @@
-import{o as t,c as a,b as e}from"./app.24159ab9.js";const n='{"title":"Home","description":"","frontmatter":{"home":true},"relativePath":"index.md","lastUpdated":1631972163413}',l={},o=[e("h1",null,"Nicholas Yang",-1),e("p",null,"Hey, I'm Nicholas Yang.",-1)];l.render=function(e,n,l,r,i,s){return t(),a("div",null,o)};export{n as __pageData,l as default};
