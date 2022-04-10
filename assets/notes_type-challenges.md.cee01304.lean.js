fb237b7fcfab87fcafc0e3726aa3590fb5e5609c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-04-10 07:21:49 +0000

    Deploy yangss3/yangss to github.com/yangss3/yangss.git:gh-pages

---
+++ assets/notes_type-challenges.md.cee01304.lean.js
@@ -0,0 +1,1 @@
+import{_ as n,c as s,o as a,a as t}from"./app.df974142.js";const p='{"title":"Type Challenges","description":"","frontmatter":{},"relativePath":"notes/type-challenges.md","lastUpdated":1649575260638}',o={},e=[t('',23)];var c=n(o,[["render",function(n,t,p,o,c,l){return a(),s("div",null,e)}]]);export{p as __pageData,c as default};
